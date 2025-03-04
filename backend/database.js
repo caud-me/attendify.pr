e0cd9bf0c1597d2880a4749b8eb1d645f809c9ed
Caud <143205108+caud-me@users.noreply.github.com>
2025-03-04 08:43:43 +0800

    0.5.84
    REAL TIME ATTENDANCE IS HERE

--- backend/database.js
+++ backend/database.js
@@ -3,7 +3,7 @@ const $mysql = require('mysql2/promise');
 const $pool = $mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'Jamesros092607',
+  password: 'attendify',
   database: 'attendify5',
   waitForConnections: true,
   connectionLimit: 10,
